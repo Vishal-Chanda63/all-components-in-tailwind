d51b9de22100b11b88f633e2215d24823ac3fd2a
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-09-05 14:47:22 +0530

    add common sections

---
+++ tailwind.js
@@ -0,0 +1,68 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "20px",
+        },
+      },
+      colors: {
+        "baby-blue": "#935BF3",
+        "dark-gray": "#595959",
+        "off-white": "#F5F5F5",
+        "show-white": "#FCFCFC",
+        yellow: "#FFDC00",
+        "baby-pink": "#FF5D92",
+        orange: "#FF9267",
+        "sky-blue": "#4DBFF5",
+        "gray-tundora": "#484848",
+        "columbia-blue": "#A5DEF9",
+        gray: "#717171",
+        platinum: "#E3E3E3",
+        "columbia-blues": "#A0D9F4",
+        "white-low": "#FEFEFE",
+        "dark-blue": "#6133b0",
+        "dark-black": "#00090A",
+        grey: "#d1d1d1",
+        "light-grey": "#d9d9d9",
+        green: "#359493",
+        "light-green": "#ebf4f4",
+        "dark-gray": "#343434",
+
+      },
+      fontSize: {
+        "custom-5xl": "54px",
+        "custom-4xl": "44px",
+        "custom-3xl": "30px",
+      },
+
+
+      lineHeight: {
+        100: "100%",
+        113: "113%",
+        129: "129%",
+        130: "130%",
+        125: "125%",
+        150: "150%",
+        150: "160%",
+        138: "138%",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+
+      boxShadow: {
+        dropdown: "0px 4px 70px 0px #0000002E",
+        testimonials: "0px 0px 0px 5px #FFFFFF",
+        backtotop: "0px 0px 15.3px #359493",
+
+      },
+      screens: {
+        xs: "425px",
+        "custom-xl": "1410px",
+      },
+
+    },
+  },
+};
